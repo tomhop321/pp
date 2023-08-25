476f7669e8c7e371882ba6ee9b36d935b933e3af
tomhop321 <85928808+tomhop321@users.noreply.github.com>
2023-08-25 22:02:27 +0200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 3000,
-  database: process.env.DATABASE || 'mongodb://localhost:27017/node-kurs',
+  database: process.env.DATABASE || 'mongodb+srv://adam:<tajne>@cluster0.bw7rqub.mongodb.net/?retryWrites=true&w=majority',
   sessionKeySecret: process.env.SESSION_KEY_SECRET,
   ssl: process.env.SSL || false,
 };
